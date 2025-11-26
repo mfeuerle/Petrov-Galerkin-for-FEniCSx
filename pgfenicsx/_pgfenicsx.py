316e7cc966309bab42d70b6bc5eae81c84602eed
mfeuerle <moritz.feuerle@t-online.de>
2025-11-26 15:55:15 +0100

    Moved some functionality from dirichletbc to DirichletBC

--- pgfenicsx/_pgfenicsx.py
+++ pgfenicsx/_pgfenicsx.py
@@ -11,7 +11,7 @@ from dolfinx.fem.function import FunctionSpace
 from dolfinx.mesh import exterior_facet_indices
 
 from typing import Callable, TypeAlias
-BoundaryFunctionType: TypeAlias = Function | Constant | Expression | Number | Callable 
+BoundaryFunctionType: TypeAlias = Function | Constant | Expression | Number | Callable
 
 
 def as_numpy_vector(x, dtype=None):
@@ -23,18 +23,37 @@ def as_numpy_vector(x, dtype=None):
 class DirichletBC:
     r"""Class representing a Dirichlet boundary condition."""
     
-    def __init__(self, function_space: FunctionSpace, values: np.ndarray[float], dofs: np.ndarray[int]):
-        r"""Create a Dirichlet boundary condition.
+    def __init__(self, function_space: FunctionSpace, u: BoundaryFunctionType | np.ndarray[float], dofs: np.ndarray[int]):
+        r"""Create a Dirichlet boundary condition restricting the ``dofs`` of ``function_space`` with the the values given by ``u``.
         
         Parameters
         ----------
         function_space
             The function space the boundary condition is defined on.
-        values
-            The dirichlet values to be imposed at the given dofs as array of a) same length as ``dofs``, i.e. ``values[i]`` containes the dirichlet value of dof ``dofs[i]``, or b) the length of all dofs of the function space, i.e. ``values[dofs[i]]`` containes the dirichlet value of dof ``dofs[i]``.
+        u
+            Object defining the dirichlet values at the given dofs. Can be given either the same way as in :func:`dirichletbc`, or as a discrete array directly specifying the the values at each dof, i.e. an array of a) same length as ``dofs``, i.e. ``u[i]`` containes the dirichlet value of dof ``dofs[i]``, or b) the length of all dofs of the function space, i.e. ``u[dofs[i]]`` containes the dirichlet value of dof ``dofs[i]``.
         dofs
             The dofs of the function space on which the Dirichlet condition is imposed.
         """
+        
+        if isinstance(u, Function):
+            if not u.function_space == function_space:
+                raise ValueError('Function u must be defined on the same function space as the DirichletBC')
+            values = u.x.array[dofs]
+        elif isinstance(u, Constant):
+            values = np.full(dofs.shape, u.value, dtype=default_scalar_type)
+        elif isinstance(u, Number):
+            values = np.full(dofs.shape, default_scalar_type(u))
+        else:
+            try:
+                values = as_numpy_vector(u, default_scalar_type)
+            except:
+                try:
+                    u_func = Function(function_space)
+                    u_func.interpolate(u)
+                    values = u_func.x.array[dofs]
+                except:
+                    raise TypeError('dont know what to do')
         
         dofs   = as_numpy_vector(dofs, np.int32)
         values = as_numpy_vector(values, default_scalar_type)
@@ -53,7 +72,7 @@ class DirichletBC:
         self.fixed_dofs: np.ndarray[int] = dofs[idx]
         """Fixed dofs corresponding to the dirichlet values"""
         self.values: np.ndarray[float] = values[idx]
-        """Dirichlet values"""
+        """Discrete dirichlet values, i.e. ``values[i]`` is the dirichlet value at dof ``fixed_dofs[i]``."""
         self.function_space: FunctionSpace = function_space
         """Function space the boundary condition is defined on."""
         self._free_dofs = None
@@ -62,7 +81,7 @@ class DirichletBC:
         
     @property
     def free_dofs(self) -> np.ndarray[int]:
-        """Free dofs (no Dirichlet condition is applied)."""
+        """Free dofs where no dirichlet condition is applied."""
         if self._free_dofs is None:
             free_dofs = np.ones(self.ndofs, dtype=bool)
             free_dofs[self.fixed_dofs] = False
@@ -70,10 +89,10 @@ class DirichletBC:
         return self._free_dofs
 
       
-def dirichletbc(function_space: FunctionSpace, u: BoundaryFunctionType, bdry_facets: np.ndarray[int] | None = None) -> DirichletBC:
-    r"""Create a Dirichlet boundary condition for ``function_space`` on the boundary part ``bdry_facets`` with the boundary values given by ``u``.
+def dirichletbc(function_space: FunctionSpace, u: BoundaryFunctionType, facets: np.ndarray[int] | None = None) -> DirichletBC:
+    r"""Create a Dirichlet boundary condition for ``function_space`` on the boundary part ``facets`` with the boundary values given by ``u``.
     
-    This function operates on **facets not dofs**, unlike :func:`dolfinx.fem.dirichletbc`. This decision was made to as for Petrov-Galerkin formulations, the dofs of the test and trial space may differ, while the facets belong to the mesh and are thus independend of the space (thus avoiding potential hard to debug problems by defining dirichlet conditions for one space while using the dofs of another). 
+    This function operates on **facets not dofs**, unlike :func:`dolfinx.fem.dirichletbc`. This decision was made conserning Petrov-Galerkin formulations: the dofs of the test and trial space may differ, while the facets belong to the mesh and are thus independend of the space (thus avoiding potential hard to debug errors by defining dirichlet condition for one space while using the facet dofs generated based on another space). 
     If one wants to create a dirichlet boundary condition based on dofs, one can always use :class:`DirichletBC` directly. 
     
     Parameters
@@ -82,29 +101,14 @@ def dirichletbc(function_space: FunctionSpace, u: BoundaryFunctionType, bdry_fac
         The function space the boundary condition is defined on.
     u
         The boundary value. Can be a number, :class:`dolfinx.fem.Constant`, :class:`dolfinx.fem.Function` or any object that can be interpolated into a :class:`dolfinx.fem.Function`.
-    bdry_facets
+    facets
         The boundary facets where the Dirichlet condition is applied. If ``None``, all exterior facets are used.
     """
    
-    if bdry_facets is None:
-        bdry_facets = exterior_facet_indices(function_space.mesh.topology)
+    if facets is None:
+        facets = exterior_facet_indices(function_space.mesh.topology)
     
-    bdry_facets = as_numpy_vector(bdry_facets, np.int32)
-    bdry_dofs = locate_dofs_topological(function_space, function_space.mesh.topology.dim-1, bdry_facets)
+    facets = as_numpy_vector(facets, np.int32)
+    bdry_dofs = locate_dofs_topological(function_space, function_space.mesh.topology.dim-1, facets)
     
-    if isinstance(u, Function):
-        if not u.function_space == function_space:
-            raise ValueError('Function u must be defined on the same function space as the DirichletBC')
-        values = as_numpy_vector(u.x.array[bdry_dofs],default_scalar_type)
-    elif isinstance(u, Constant):
-        values = np.full(bdry_dofs.shape, u.value, dtype=default_scalar_type)
-    elif isinstance(u, Number):
-        values = np.full(bdry_dofs.shape, default_scalar_type(u))
-    else:
-        try:
-            u_func = Function(function_space)
-            u_func.interpolate(u)
-            values = as_numpy_vector(u_func.x.array[bdry_dofs], default_scalar_type)
-        except:
-            raise TypeError('dont know what to do')
-    return DirichletBC(function_space, values, bdry_dofs)
+    return DirichletBC(function_space, u, bdry_dofs)
--- tests/test_dirichletbc_creation.py
+++ tests/test_dirichletbc_creation.py
@@ -1,10 +1,3 @@
-import pathlib
-import sys
-
-from ufl import functionspace
-
-sys.path.append(str(pathlib.Path(__file__).parents[1]))
-
 import numpy as np
 import pytest
 
@@ -12,8 +5,10 @@ from mpi4py import MPI
 from dolfinx.mesh import create_unit_square, locate_entities_boundary, exterior_facet_indices
 from dolfinx.fem import functionspace, Constant, Function, locate_dofs_topological
 from dolfinx import default_scalar_type
-from numbers import Number
 
+import pathlib
+import sys
+sys.path.append(str(pathlib.Path(__file__).parents[1]))
 from pgfenicsx import DirichletBC, dirichletbc
 
 
@@ -21,146 +16,127 @@ mesh = create_unit_square(MPI.COMM_WORLD, 4, 4)
 mesh.topology.create_connectivity(mesh.topology.dim-1, mesh.topology.dim)
 U = functionspace(mesh, ("Lagrange", 1))
         
-sub_marker  = lambda x: np.isclose(x[0], 0)
-sub_facets = locate_entities_boundary(mesh, dim=mesh.topology.dim-1, marker=sub_marker)
-sub_dofs_fixed = np.sort(locate_dofs_topological(U, mesh.topology.dim-1, sub_facets))
-sub_dofs_free = np.ones(U.dofmap.index_map.size_global, dtype=bool)
-sub_dofs_free[sub_dofs_fixed] = False
-sub_dofs_free = np.sort(np.where(sub_dofs_free)[0])
-
-full_facets = exterior_facet_indices(mesh.topology)
-full_dofs_fixed = np.sort(locate_dofs_topological(U, mesh.topology.dim-1, full_facets))
-full_dofs_free = np.ones(U.dofmap.index_map.size_global, dtype=bool)
-full_dofs_free[full_dofs_fixed] = False
-full_dofs_free = np.sort(np.where(full_dofs_free)[0])
+marker  = lambda x: np.isclose(x[0], 0)
+facets = locate_entities_boundary(mesh, dim=mesh.topology.dim-1, marker=marker)
+fixed_dofs = np.sort(locate_dofs_topological(U, mesh.topology.dim-1, facets))
 
 
-full_facets = exterior_facet_indices(mesh.topology)
-
-
-def _test(bc, values, fixed_dofs, free_dofs, space):
+def _test_bc(bc, values, fixed_dofs, free_dofs, space):
     assert np.allclose(bc.values, values[fixed_dofs])
     assert np.array_equal(bc.fixed_dofs, fixed_dofs)
     assert np.array_equal(bc.free_dofs, free_dofs)
     assert bc.function_space == space
     
 
-
-class _Test_dirichletbc_interface:
+class _Test_DirichletBC_interface:
     
     u = None  # to be defined in subclasses
+    values = None
+    fixed_dofs = None 
     expected_error = None
     
-    sub_facets = sub_facets
-    sub_dofs_fixed   = sub_dofs_fixed
-    sub_dofs_free = sub_dofs_free
-    full_facets = full_facets
-    full_dofs_fixed   = full_dofs_fixed
-    full_dofs_free = full_dofs_free
-    
-    _values = None
+    _free_dofs = None
     @property
-    def values(self):
-        if self._values is None:
-            if isinstance(self.u, Function):
-                values = self.u.x.array
-            elif isinstance(self.u, Constant):
-                values = np.full(U.dofmap.index_map.size_global, self.u.value, dtype= default_scalar_type)
-            elif isinstance(self.u, Number):
-                values = np.full(U.dofmap.index_map.size_global, default_scalar_type(self.u))
-            else:
-                _u = Function(U)
-                _u.interpolate(self.u)
-                values = _u.x.array
-            self._values = values
-        return self._values
+    def free_dofs(self):
+        if self._free_dofs is None:
+            free_dofs = np.ones(U.dofmap.index_map.size_global, dtype=bool)
+            free_dofs[self.fixed_dofs] = False
+            self._free_dofs = np.sort(np.where(free_dofs)[0])
+        return self._free_dofs
     
-    def _test(self, facets, fixed_dofs, free_dofs):
-        try:
-            if facets is None:
-                bc = dirichletbc(U, self.u)
-            else:
-                bc = dirichletbc(U, self.u, facets)
-        except self.expected_error:
-            return
-        _test(bc, self.values, fixed_dofs, free_dofs, U)
-        
-    def test_fullboundary(self):
-        self._test(self.full_facets, self.full_dofs_fixed, self.full_dofs_free)
-        
-    def test_subboundary(self):
-        self._test(self.sub_facets, self.sub_dofs_fixed, self.sub_dofs_free)
-        
-    def test_noboundary(self):
-        self._test(None, self.full_dofs_fixed, self.full_dofs_free)
+    def test(self):
+        if self.expected_error is not None:
+            with pytest.raises(self.expected_error):
+                bc = DirichletBC(U, self.u, self.fixed_dofs)
+        else:
+            bc = DirichletBC(U, self.u, self.fixed_dofs)
+            _test_bc(bc, self.values, self.fixed_dofs, self.free_dofs, U)
 
     
-class Test_dirichletbc_Number(_Test_dirichletbc_interface):
-    u = 2.0
+class Test_DirichletBC_Number(_Test_DirichletBC_interface):
+    u = 2
+    values = np.full(U.dofmap.index_map.size_global, 2.0, dtype=default_scalar_type)
+    fixed_dofs = fixed_dofs
         
-class Test_dirichletbc_Constant(_Test_dirichletbc_interface):
-    u = Constant(mesh, 3.0)
+class Test_DirichletBC_Constant(_Test_DirichletBC_interface):
+    u = Constant(mesh, 3.4)
+    values = np.full(U.dofmap.index_map.size_global, 3.4, dtype=default_scalar_type)
+    fixed_dofs = fixed_dofs
     
-class Test_dirichletbc_Callable(_Test_dirichletbc_interface):
+class Test_DirichletBC_Callable(_Test_DirichletBC_interface):
     u = lambda self,x: x[0] + x[1]
-    
-class Test_dirichletbc_Function(_Test_dirichletbc_interface):
+    values = Function(U)
+    values.interpolate(lambda x: x[0] + x[1])
+    values = values.x.array    
+    fixed_dofs = fixed_dofs
+class Test_DirichletBC_Function(_Test_DirichletBC_interface):
     u = Function(U)
     u.interpolate(lambda x: x[0] * x[1])
+    values = u.x.array
+    fixed_dofs = fixed_dofs
     
-class Test_dirichletbc_wrong_space(_Test_dirichletbc_interface):
+class Test_DirichletBC_Array_short(_Test_DirichletBC_interface):
+    values = np.linspace(0, 1, U.dofmap.index_map.size_global)
+    u = values[fixed_dofs]
+    fixed_dofs = fixed_dofs
+    
+class Test_DirichletBC_Array_full(_Test_DirichletBC_interface):
+    values = np.linspace(0, 1, U.dofmap.index_map.size_global)
+    u = values
+    fixed_dofs = fixed_dofs
+    
+class Test_DirichletBC_Array_wrong_length(_Test_DirichletBC_interface):
+    values = np.linspace(0, 1, U.dofmap.index_map.size_global)
+    u = np.concatenate([values, values[:1]])
+    fixed_dofs = fixed_dofs
+    expected_error = ValueError
+    
+class Test_DirichletBC_duplicate_dofs(_Test_DirichletBC_interface):
+    values = np.linspace(0, 1, U.dofmap.index_map.size_global)
+    u = np.concatenate([values, values[fixed_dofs[:1]]])
+    fixed_dofs = np.concatenate([fixed_dofs, fixed_dofs[:1]])
+    expected_error = ValueError
+    
+class Test_DirichletBC_wrong_space(_Test_DirichletBC_interface):
     u = Function(functionspace(mesh, ("Lagrange", 1)))
     u.interpolate(lambda x: x[0] + x[1])    
+    values = u.x.array
+    fixed_dofs = fixed_dofs
     expected_error = ValueError
     
 
 
+free_dofs = np.ones(U.dofmap.index_map.size_global, dtype=bool)
+free_dofs[fixed_dofs] = False
+free_dofs = np.sort(np.where(free_dofs)[0])
+            
+facets_all = exterior_facet_indices(mesh.topology)
+fixed_dofs_all = np.sort(locate_dofs_topological(U, mesh.topology.dim-1, facets_all))
+free_dofs_all = np.ones(U.dofmap.index_map.size_global, dtype=bool)
+free_dofs_all[fixed_dofs_all] = False
+free_dofs_all = np.sort(np.where(free_dofs_all)[0])
 
-
-values = np.linspace(0, 1, U.dofmap.index_map.size_global)
-dofes_fixed = sub_dofs_fixed
-dofs_free   = sub_dofs_free
-
-def test_DirichletBC_dof_length():
-    bc = DirichletBC(U, values[dofes_fixed], dofes_fixed)
-    _test(bc, values, dofes_fixed, dofs_free, U)
+def test_dirichletbc_facets_given():
+    values = np.linspace(0, 1, U.dofmap.index_map.size_global)
+    bc = dirichletbc(U, values, facets=facets)
+    _test_bc(bc, values, fixed_dofs, free_dofs, U)
     
-def test_DirichletBC_space_length():
-    bc = DirichletBC(U, values, dofes_fixed)
-    _test(bc, values, dofes_fixed, dofs_free, U)
-    
-def test_DirichletBC_wrong_length():
-    with pytest.raises(ValueError):
-        DirichletBC(U, values[:len(dofs_free)+1], dofs_free)
-        
-def test_DirichletBC_duplicate_dofs():
-    with pytest.raises(ValueError):
-        dofs = np.concatenate([dofes_fixed, dofes_fixed[:1]])
-        DirichletBC(U, values[dofs], dofs)
+def test_dirichletbc_no_facets():
+    values = np.linspace(0, 1, U.dofmap.index_map.size_global)
+    bc = dirichletbc(U, values)
+    _test_bc(bc, values, fixed_dofs_all, free_dofs_all, U)
  
 
 # if __name__ == "__main__":
-#     T = Test_dirichletbc_Function()
-#     T.test_fullboundary()
-#     T.test_subboundary()
-#     T.test_noboundary()
+#     Test_DirichletBC_Function().test()
+#     Test_DirichletBC_Constant().test()
+#     Test_DirichletBC_Number().test()
+#     Test_DirichletBC_Callable().test()
+#     Test_DirichletBC_Array_short().test()
+#     Test_DirichletBC_Array_full().test()
+#     Test_DirichletBC_Array_wrong_length().test()
+#     Test_DirichletBC_wrong_space().test()
+#     Test_DirichletBC_duplicate_dofs().test()
     
-#     T = Test_dirichletbc_Callable()
-#     T.test_fullboundary()
-#     T.test_subboundary()
-#     T.test_noboundary()
-    
-#     T = Test_dirichletbc_Constant()
-#     T.test_fullboundary()
-#     T.test_subboundary()
-#     T.test_noboundary() 
-    
-#     T = Test_dirichletbc_Number()
-#     T.test_fullboundary()
-#     T.test_subboundary()
-#     T.test_noboundary()
-    
-#     test_DirichletBC_dof_length()
-#     test_DirichletBC_space_length()
-#     test_DirichletBC_wrong_length()
-#     test_DirichletBC_duplicate_dofs()
+#     test_dirichletbc_facets_given()
+#     test_dirichletbc_no_facets()
