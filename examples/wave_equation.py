89bc90c7c194b4d4d3f88f0a25fdd734931688aa
mfeuerle <moritz.feuerle@t-online.de>
2025-11-27 20:34:45 +0100

    added small test example solving the wave equation

---
+++ examples/wave_equation.py
@@ -0,0 +1,111 @@
+import numpy as np
+from scipy.sparse.linalg import spsolve
+from mpi4py import MPI
+from dolfinx import mesh, fem
+
+import ufl
+from enum import IntEnum
+
+from pgfenicsx import setup_boundary_meshtags, dirichletbc, assemble_system
+
+
+np.set_printoptions(edgeitems=30, linewidth=100000, precision=2)
+
+I     = [0.0, 3.0]
+Omega = [0.0, 2.0]
+
+nx = 15
+nt = np.ceil((I[1]-I[0])/((Omega[1]-Omega[0])/(nx+1))).astype('int')
+
+msh = mesh.create_rectangle(MPI.COMM_WORLD, [[I[0], Omega[0]], [I[1], Omega[1]]], [nt, nx])
+msh.topology.create_connectivity(msh.topology.dim-1, msh.topology.dim)
+
+tx = ufl.SpatialCoordinate(msh)
+n  = ufl.FacetNormal(msh)
+
+u_exact = lambda tx: np.sin(np.pi*tx[0])*tx[1] + 1  # +1 to avoid zero boundary values
+f  = -ufl.pi**2 * ufl.sin(ufl.pi*tx[0])*tx[1]   # right-hand side
+u0 = u_exact                                    # dirichlet BC at initial time    
+u1 = ufl.pi * ufl.cos(ufl.pi*tx[0])*tx[1]       # neuman BC at initial time
+g0 = u_exact                                    # dirichlet BC at spatial boundary
+
+
+class bdry_prt(IntEnum): t0,T,gamma = range(3)
+
+bdry  = [(bdry_prt.t0, lambda tx: np.isclose(tx[0], I[0])),  # initial time
+         (bdry_prt.T,  lambda tx: np.isclose(tx[0], I[1])),  # terminal time
+         (bdry_prt.gamma,)]                                  # boundary in space (= the remaining bounary parts)
+
+bdry_tagged = setup_boundary_meshtags(msh, bdry)
+ds = ufl.Measure("ds", domain=msh, subdomain_data=bdry_tagged)
+
+U = fem.functionspace(msh, ("Lagrange", 1))
+V = fem.functionspace(msh, ("Lagrange", 1))
+
+bcs = [ dirichletbc(U, u0, bdry_tagged.find(bdry_prt.t0)),
+        dirichletbc(U, g0, bdry_tagged.find(bdry_prt.gamma)),
+        dirichletbc(V, 0, bdry_tagged.find(bdry_prt.T)),
+        dirichletbc(V, 0, bdry_tagged.find(bdry_prt.gamma))]
+
+u = ufl.TrialFunction(U)
+v = ufl.TestFunction(V)
+
+A =  - ufl.inner(ufl.grad(u)[0],ufl.grad(v)[0]) * ufl.dx
+for i in range(1,len(tx)):
+    A += ufl.inner(ufl.grad(u)[i],ufl.grad(v)[i]) * ufl.dx
+
+l = f*v*ufl.dx + u1*v*ds(bdry_prt.t0)
+
+A1,l1 = assemble_system((A,l), bcs, include_bc=True)
+u1 = spsolve(A1, l1)
+
+A2,l2,trial_bc = assemble_system((A,l), bcs, include_bc=False)
+u2 = np.empty((trial_bc.ndofs,))
+u2[trial_bc.free_dofs] = spsolve(A2, l2)
+u2[trial_bc.fixed_dofs] = trial_bc.values
+
+u_exact_discrete = fem.Function(U)
+u_exact_discrete.interpolate(u_exact)
+
+print(np.linalg.norm(u1 - u_exact_discrete.x.array, ord=np.inf))
+print(np.linalg.norm(u2 - u_exact_discrete.x.array, ord=np.inf))
+
+    
+try:
+    import pyvista
+    from dolfinx import plot
+    from pathlib import Path
+    
+    results_folder = Path(__file__).parent / f'plots_{Path(__file__).stem}'
+    
+    def plot_pyvista(u,name, plotter):
+        cells, types, x = plot.vtk_mesh(V)
+        grid = pyvista.UnstructuredGrid(cells, types, x)
+        grid.point_data["u"] = u
+        grid.set_active_scalars("u")
+        grid.rotate_z(180, inplace=True)
+        plotter.add_mesh(grid.warp_by_scalar(), show_edges=True)
+        plotter.show_grid(xtitle='t', ytitle='x', ztitle='u(t,x)')
+        plotter.add_text(name)
+        
+    
+    plotter = pyvista.Plotter(shape=(1,3))
+    plotter.subplot(0, 0)
+    plot_pyvista(u_exact_discrete.x.array, "u_exact", plotter)
+    plotter.subplot(0, 1)
+    plot_pyvista(u1, "u1", plotter)
+    plotter.subplot(0, 2)
+    plot_pyvista(u2, "u2", plotter)
+    # plotter.subplot(1, 1)
+    # plot_pyvista(u1 - u_exact_discrete.x.array, "error u1", plotter)
+    # plotter.subplot(1, 2)
+    # plot_pyvista(u2 - u_exact_discrete.x.array, "error u2", plotter)
+    
+    if pyvista.OFF_SCREEN:
+        plotter.screenshot(results_folder / f".png")
+    else:
+        plotter.show()
+
+except ModuleNotFoundError:
+    print("'pyvista' is required to visualise the solution.")
+    print("To install pyvista with pip: 'python3 -m pip install pyvista' or conda: 'conda install -c conda-forge pyvista'.")
